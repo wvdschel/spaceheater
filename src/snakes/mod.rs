f0b9e0db6388a42d29f5e7409651a1c8b8cb1e55
Wim Vander Schelden <lists@fixnum.org>
2022-11-29 14:16:54 +0100

    customized spaceheater3

--- src/snakes/mod.rs
+++ src/snakes/mod.rs
@@ -5,7 +5,7 @@ pub use simple::SimpleSnake;
 pub use spaceheater3::Spaceheater3;
 use std::collections::HashMap;
 
-use crate::{logic, Battlesnake};
+use crate::{logic, protocol::Customizations, Battlesnake};
 
 pub fn snakes() -> HashMap<String, Box<dyn Battlesnake + Sync + Send>> {
     let mut snakes = HashMap::<String, Box<dyn Battlesnake + Sync + Send>>::new();
@@ -14,7 +14,11 @@ pub fn snakes() -> HashMap<String, Box<dyn Battlesnake + Sync + Send>> {
         "spaceheater3".to_string(),
         Box::new(Spaceheater3::new(
             logic::scoring::tournament_score,
-            None,
+            Some(Customizations {
+                color: "#DA2C43".to_string(),
+                head: "smile".to_string(),
+                tail: "nr-booster".to_string(),
+            }),
             false,
         )),
     );
@@ -22,7 +26,11 @@ pub fn snakes() -> HashMap<String, Box<dyn Battlesnake + Sync + Send>> {
         "spaceheater3_mc".to_string(),
         Box::new(Spaceheater3::new(
             logic::scoring::tournament_score,
-            None,
+            Some(Customizations {
+                color: "#FF2400".to_string(),
+                head: "workout".to_string(),
+                tail: "rocket".to_string(),
+            }),
             true,
         )),
     );
