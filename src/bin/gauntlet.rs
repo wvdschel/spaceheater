8b03a98cc820d080997eefb25294274aeea3c21d
Wim Vander Schelden <lists@fixnum.org>
2022-12-10 10:09:20 +0100

    skip empty lines from champions.txt

--- src/bin/gauntlet.rs
+++ src/bin/gauntlet.rs
@@ -1,5 +1,7 @@
 use topsnek::{logic::scoring::winter, util::gauntlet::Gauntlet};
 
+const CHAMPIONS: &str = include_str!("champions.txt");
+
 fn main() {
     let mut g = Gauntlet::new(&[
         "--shrinkEveryNTurns",
@@ -13,6 +15,9 @@ fn main() {
     ]);
     if CHAMPIONS != "" {
         for (i, champ) in CHAMPIONS.split("\n").enumerate() {
+            if champ == "" {
+                continue;
+            }
             let cfg = winter::Config::<{ u16::MAX }>::try_from(champ).unwrap();
             g.add_contestant(format!("champion_{}", i).as_str(), cfg)
         }
@@ -22,5 +27,3 @@ fn main() {
         g.new_round(4);
     }
 }
-
-const CHAMPIONS: &str = include_str!("champions.txt");
